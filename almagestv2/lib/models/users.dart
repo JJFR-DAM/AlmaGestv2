9e7130b5c30c1557ab5144cfab4c6fbdab9ba6fb
Juan Jesús Fernández Rodríguez <alba-juanjesus@hotmail.es>
2023-03-22 09:24:24 +0100

    22/03/2023
    Avance en las ventanas necesarias para la primera entrega y los modelos respectivos.

---
+++ almagestv2/lib/models/users.dart
@@ -0,0 +1,81 @@
+class Users {
+  bool? success;
+  List<UserData>? data;
+  String? message;
+
+  Users({this.success, this.data, this.message});
+
+  Users.fromJson(Map<String, dynamic> json) {
+    success = json['success'];
+    if (json['data'] != null) {
+      data = <UserData>[];
+      json['data'].forEach((v) {
+        data!.add(UserData.fromJson(v));
+      });
+    }
+    message = json['message'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['success'] = success;
+    if (this.data != null) {
+      data['data'] = this.data!.map((v) => v.toJson()).toList();
+    }
+    data['message'] = message;
+    return data;
+  }
+}
+
+class UserData {
+  int? id;
+  String? firstname;
+  String? secondname;
+  int? actived;
+  String? email;
+  String? type;
+  int? emailConfirmed;
+  int? deleted;
+  String? createdAt;
+  String? updatedAt;
+
+  UserData(
+      {this.id,
+      this.firstname,
+      this.secondname,
+      this.actived,
+      this.email,
+      this.type,
+      this.emailConfirmed,
+      this.deleted,
+      this.createdAt,
+      this.updatedAt});
+
+  UserData.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    firstname = json['firstname'];
+    secondname = json['secondname'];
+    actived = json['actived'];
+    email = json['email'];
+    type = json['type'];
+    emailConfirmed = json['email_confirmed'];
+    deleted = json['deleted'];
+    createdAt = json['created_at'];
+    updatedAt = json['created_at'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['id'] = id;
+    data['firstname'] = firstname;
+    data['secondname'] = secondname;
+    data['actived'] = actived;
+    data['email'] = email;
+    data['type'] = type;
+    data['email_confirmed'] = emailConfirmed;
+    data['deleted'] = deleted;
+    data['created_at'] = createdAt;
+    data['updated_at'] = updatedAt;
+    return data;
+  }
+}
